d8e02aa4e89494fc7507ded95ce8155371e19fcc
Takashi Matsuoka <matsujirushi@live.jp>
2021-04-30 06:27:06 +0100

    Use mj-azure-iot-pnp-device

--- src/reterminal_aziot_example.py
+++ src/reterminal_aziot_example.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
 import asyncio
+import base64
+import hashlib
+import hmac
 import json
 import os
 import sys
+import time
+
 import reterminal.core as rt
 import reterminal.acceleration as rt_accel
 import reterminal.button as rt_btn
+
 from azure.iot.device.aio import ProvisioningDeviceClient
 from azure.iot.device.aio import IoTHubDeviceClient
-from azure.iot.device import Message, MethodResponse, exceptions
-from enum import Enum
-import base64
-import hmac
-import hashlib
+from azure.iot.device import exceptions
 
-FIRMWARE_VERSION = "1.0"
+from mj_azure_iot_pnp_device.device import IoTHubDeviceClient as MjClient
+import mj_azure_iot_pnp_device.contents as MjCont
+from varname import nameof
+
+FIRMWARE_VERSION = "1.1"
 
 LED_OFF = 1
 LED_ON = 2
@@ -33,83 +39,59 @@ SYMMETRIC_KEY = os.getenv("IOTHUB_DEVICE_DPS_DEVICE_KEY")
 DEVICE_CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 
 
-# Properties
-properties = []
+class RingBuzzerCommand(MjCont.Command):
+    def handler(self, payload):
+        if payload and type(payload) == int:
+            duration = payload
+            print(f"ringBuzzer command received. duration={duration}")
+            rt.buzzer = True
+            time.sleep(duration / 1000)
+            rt.buzzer = False
+
+            return 200, "ringBuzzer succeeded"
+
+        return 400, "ringBuzzer error"
 
 
-class PropertyType(Enum):
-    Writable = 1
-    ReadOnly = 2
+class StaLedGreenProperty(MjCont.WritableProperty):
+    @property
+    def value(self):
+        return LED_ON if rt.sta_led_green == True else LED_OFF
+
+    @value.setter
+    def value(self, value):
+        rt.sta_led_green = value == LED_ON
 
 
-class TwinProperties:
-    def __init__(self, propertyName, propertyType, callback=None):
-        self.name = propertyName
-        self.type = propertyType
-        self.callback = callback
+class StaLedRedProperty(MjCont.WritableProperty):
+    @property
+    def value(self):
+        return LED_ON if rt.sta_led_red == True else LED_OFF
+
+    @value.setter
+    def value(self, value):
+        rt.sta_led_red = value == LED_ON
 
 
-def processLed(key, value):
+class UsrLedProperty(MjCont.WritableProperty):
+    @property
+    def value(self):
+        return LED_ON if rt.usr_led == True else LED_OFF
 
-    print(f"Processing {key} Value {value}")
-    ack_code = 200
-
-    if key == "ledStaGreen":
-        rt.sta_led_green = True if value == LED_ON else False
-    elif key == "ledStaRed":
-        rt.sta_led_red = True if value == LED_ON else False
-    elif key == "usrLed":
-        rt.usr_led = True if value == LED_ON else False
-    else:
-        ack_code = 400
-
-    return ack_code
+    @value.setter
+    def value(self, value):
+        rt.usr_led = value == LED_ON
 
 
-def processTelemetryInterval(key, value):
+class TelemetryIntervalProperty(MjCont.WritableProperty):
+    @property
+    def value(self):
+        return telemetry_interval
 
-    global telemetry_interval
-    telemetry_interval = value
-    return 200
-
-
-properties.append(TwinProperties("ledStaGreen", PropertyType.Writable, processLed))
-properties.append(TwinProperties("ledStaRed", PropertyType.Writable, processLed))
-properties.append(TwinProperties("usrLed", PropertyType.Writable, processLed))
-properties.append(TwinProperties("firmwareVer", PropertyType.ReadOnly))
-properties.append(TwinProperties("telemetryInterval", PropertyType.Writable, processTelemetryInterval))
-
-
-def check_environment_variables():
-    ok = True
-
-    if SECURITY_TYPE == "DPS":
-        if DPS_DEVICE_ENDPOINT_HOST is None:
-            print("ERROR: The environment variable DPS_DEVICE_ENDPOINT_HOST is invalid.", file=sys.stderr)
-            ok = False
-
-        if ID_SCOPE is None:
-            print("ERROR: The environment variable ID_SCOPE is invalid.", file=sys.stderr)
-            ok = False
-
-        if REGISTRATION_ID is None:
-            print("ERROR: The environment variable REGISTRATION_ID is invalid.", file=sys.stderr)
-            ok = False
-
-        if SYMMETRIC_KEY is None:
-            print("ERROR: The environment variable SYMMETRIC_KEY is invalid.", file=sys.stderr)
-            ok = False
-
-    elif SECURITY_TYPE == "connectionString":
-        if DEVICE_CONNECTION_STRING is None:
-            print("ERROR: The environment variable DEVICE_CONNECTION_STRING is invalid.", file=sys.stderr)
-            ok = False
-
-    else:
-        print("ERROR: The environment variable SECURITY_TYPE is invalid.", file=sys.stderr)
-        ok = False
-
-    return ok
+    @value.setter
+    def value(self, value):
+        global telemetry_interval
+        telemetry_interval = value
 
 
 def generate_device_key(device_id, group_symmetric_key):
@@ -145,134 +127,26 @@ async def provision_device(provisioning_host, id_scope, registration_id, symmetr
     return None
 
 
-async def send_writable_property_confirm(
-    name, value, ack_code, ack_version, description="Completed"
-):
-    print(f'Reported Property {name} value {value} ac {ack_code} av {ack_version}')
-    prop_dict = {}
-    prop_dict[name] = {
-        "value": value,
-        "ac": ack_code,
-        "av": ack_version,
-        "ad": description,
-    }
-
-    await device_client.patch_twin_reported_properties(prop_dict)
-
-
-async def direct_method_handler(command_request):
-
-    print("Command request (%s) received with payload %s" % (command_request.name, command_request.payload))
-
-    if command_request.name == 'ringBuzzer':
-        values = {}
-        if not command_request.payload:
-            print("Payload was empty.")
-        else:
-            values = command_request.payload
-
-        await ring_buzzer_handler(values)
-
-        response_status = 200
-        response_payload = ring_buzzer_response(values)
-
-        command_response = MethodResponse.create_from_method_request(
-            command_request, response_status, response_payload
-        )
-
-        try:
-            await device_client.send_method_response(command_response)
-        except Exception:
-            print(f"responding to the {command_request.name} command failed.")
-
-
-async def twin_patch_handler(patch):
-
-    ack_code = 200
-
-    for key in patch.keys():
-        if key == '$version':
-            continue
-
-        prop = next((x for x in properties if x.name == key), None)
-
-        if prop == None:
-            ack_code = 400
-        elif prop.type == PropertyType.ReadOnly:
-            ack_code = 400
-        else:
-            if prop.callback != None:
-                ack_code = prop.callback(key, patch[key])
-
-        await send_writable_property_confirm(key, patch[key], ack_code, patch["$version"])
-
-
-async def send_telemetry(device_client, telemetry_msg):
-    msg = Message(json.dumps(telemetry_msg))
-    msg.content_encoding = "utf-8"
-    msg.content_type = "application/json"
-    await device_client.send_message(msg)
-
-
-async def send_telemetry_acceleration_loop():
-    global device_client
-
-    while True:
-        if "x" in acceleration and "y" in acceleration and "z" in acceleration:
-            print("Sending telemetry for acceleration.")
-            await send_telemetry(
-                device_client,
-                {
-                    "acceleration": {
-                        "x": acceleration["x"],
-                        "y": acceleration["y"],
-                        "z": acceleration["z"],
-                    }
-                },
-            )
-
-        await asyncio.sleep(telemetry_interval)
-
-
-async def send_telemetry_button_f1():
-    print("Sending telemetry for F1 button.")
-    await send_telemetry(device_client, {"f1Button": "click"})
-
-
-async def send_telemetry_button_f2():
-    print("Sending telemetry for F2 button.")
-    await send_telemetry(device_client, {"f2Button": "click"})
-
-
-async def send_telemetry_button_f3():
-    print("Sending telemetry for F3 button.")
-    await send_telemetry(device_client, {"f3Button": "click"})
-
-
-async def send_telemetry_button_o():
-    print("Sending telemetry for O button.")
-    await send_telemetry(device_client, {"oButton": "click"})
-
-
-async def ring_buzzer_handler(values):
-    if values and type(values) == int:
-        duration = values
-        print(f"ringBuzzer command received. duration={duration}")
-        rt.buzzer = True
-        await asyncio.sleep(duration / 1000)
-        rt.buzzer = False
-
-
-def ring_buzzer_response(values):
-    return "ringBuzzer succeeded"
-
-
 def stdin_listener():
     while True:
         selection = input("Press Q to quit.\n")
         if selection == "Q" or selection == "q":
             print("Quitting...")
             break
+
+
+async def send_telemetry_acceleration_loop():
+    while True:
+        if "x" in acceleration and "y" in acceleration and "z" in acceleration:
+            print("Sending telemetry for acceleration.")
+            pnp_client.acceleration.value = {
+                "x": acceleration["x"],
+                "y": acceleration["y"],
+                "z": acceleration["z"],
+            }
+            await pnp_client.send_telemetry(nameof(pnp_client.acceleration))
+
+        await asyncio.sleep(telemetry_interval)
 
 
 async def accel_coroutine(device):
@@ -292,18 +166,54 @@ async def btn_coroutine(device):
         buttonEvent = rt_btn.ButtonEvent(event)
 
         if (buttonEvent.name, buttonEvent.value) == (rt_btn.ButtonName.F1, 1):
-            await send_telemetry_button_f1()
+            pnp_client.f1Button.value = "click"
+            await pnp_client.send_telemetry(nameof(pnp_client.f1Button))
         if (buttonEvent.name, buttonEvent.value) == (rt_btn.ButtonName.F2, 1):
-            await send_telemetry_button_f2()
+            pnp_client.f2Button.value = "click"
+            await pnp_client.send_telemetry(nameof(pnp_client.f2Button))
         if (buttonEvent.name, buttonEvent.value) == (rt_btn.ButtonName.F3, 1):
-            await send_telemetry_button_f3()
+            pnp_client.f3Button.value = "click"
+            await pnp_client.send_telemetry(nameof(pnp_client.f3Button))
         if (buttonEvent.name, buttonEvent.value) == (rt_btn.ButtonName.O, 1):
-            await send_telemetry_button_o()
+            pnp_client.oButton.value = "click"
+            await pnp_client.send_telemetry(nameof(pnp_client.oButton))
+
+
+def check_environment_variables():
+    ok = True
+
+    if SECURITY_TYPE == "DPS":
+        if DPS_DEVICE_ENDPOINT_HOST is None:
+            print("ERROR: The environment variable DPS_DEVICE_ENDPOINT_HOST is invalid.", file=sys.stderr)
+            ok = False
+
+        if ID_SCOPE is None:
+            print("ERROR: The environment variable ID_SCOPE is invalid.", file=sys.stderr)
+            ok = False
+
+        if REGISTRATION_ID is None:
+            print("ERROR: The environment variable REGISTRATION_ID is invalid.", file=sys.stderr)
+            ok = False
+
+        if SYMMETRIC_KEY is None:
+            print("ERROR: The environment variable SYMMETRIC_KEY is invalid.", file=sys.stderr)
+            ok = False
+
+    elif SECURITY_TYPE == "connectionString":
+        if DEVICE_CONNECTION_STRING is None:
+            print("ERROR: The environment variable DEVICE_CONNECTION_STRING is invalid.", file=sys.stderr)
+            ok = False
+
+    else:
+        print("ERROR: The environment variable SECURITY_TYPE is invalid.", file=sys.stderr)
+        ok = False
+
+    return ok
 
 
 async def main():
     global device_client
-    global telemetry_interval
+    global pnp_client
 
     if not check_environment_variables():
         return 1
@@ -347,65 +257,26 @@ async def main():
         print("ERROR: At least one choice needs to be made for complete functioning of this sample.", file=sys.stderr)
         return 1
 
+    # Create the pnp client.
+    pnp_client = MjClient()
+    pnp_client.acceleration = MjCont.Telemetry()
+    pnp_client.ambientLight = MjCont.Telemetry()
+    pnp_client.f1Button = MjCont.Telemetry()
+    pnp_client.f2Button = MjCont.Telemetry()
+    pnp_client.f3Button = MjCont.Telemetry()
+    pnp_client.oButton = MjCont.Telemetry()
+    pnp_client.ringBuzzer = RingBuzzerCommand()
+    pnp_client.ledStaGreen = StaLedGreenProperty()
+    pnp_client.ledStaRed = StaLedRedProperty()
+    pnp_client.usrLed = UsrLedProperty()
+    pnp_client.firmwareVer = MjCont.ReadOnlyProperty()
+    pnp_client.telemetryInterval = TelemetryIntervalProperty()
+
+    pnp_client.firmwareVer.value = FIRMWARE_VERSION
+
     # Connect the client.
-    await device_client.connect()
-
-    # Get the device twin.
-    twin = await device_client.get_twin()
-    if "$version" not in twin["desired"]:
-        print("ERROR: DeviceTwin does not contain $version.", file=sys.stderr)
-        return 1
-
-    for prop in properties:
-        value = -1
-        version = 1
-        ack_code = 200
-
-        if prop.type == PropertyType.Writable:
-
-            if prop.name in twin["desired"]:
-                value = twin["desired"][prop.name]
-                version = twin["desired"]["$version"]
-                if prop.callback != None:
-                    ack_code = prop.callback(prop.name, twin["desired"][prop.name])
-            else:
-                version = 1
-                if prop.name == "ledStaGreen":
-                    value = LED_ON if rt.sta_led_green == True else LED_OFF
-
-                elif prop.name == "ledStaRed":
-                    value = LED_ON if rt.sta_led_red == True else LED_OFF
-
-                elif prop.name == "usrLed":
-                    value = LED_ON if rt.usr_led == True else LED_OFF
-
-                elif prop.name == "telemetryInterval":
-                    value = telemetry_interval
-
-            await send_writable_property_confirm(prop.name, value, ack_code, version)
-
-        elif prop.type == PropertyType.ReadOnly:
-
-            sendReported = False
-
-            if prop.name == "firmwareVer":
-                if prop.name in twin["reported"]:
-                    value = twin["reported"]["firmwareVer"]
-
-                    if FIRMWARE_VERSION != value:
-                        value = FIRMWARE_VERSION
-                        sendReported = True
-                else:
-                    value = FIRMWARE_VERSION
-                    sendReported = True
-
-                if sendReported:
-                    await device_client.patch_twin_reported_properties(
-                        {"firmwareVer": FIRMWARE_VERSION}
-                    )
-
-    device_client.on_twin_desired_properties_patch_received = twin_patch_handler
-    device_client.on_method_request_received = direct_method_handler
+    pnp_client.set_iot_hub_device_client(device_client)
+    await pnp_client.connect()
 
     # Assign acceleration and button events.
     accel_device = rt.get_acceleration_device()
